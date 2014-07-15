a08dbca771f6e52a165ff30ec926812c551a4086
dbushkanets <dbushkanets@harryfox.com>
2014-07-14 20:14:14 -0400

    added home controller, created home#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,47 @@
 GetGreenir::Application.routes.draw do
+
+  resources :providers
+
+  resources :electric_packages
+
+  root to: "home#index"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   Prefix Verb   URI Pattern                           Controller#Action
+#             providers GET    /providers(.:format)                  providers#index
+#                       POST   /providers(.:format)                  providers#create
+#          new_provider GET    /providers/new(.:format)              providers#new
+#         edit_provider GET    /providers/:id/edit(.:format)         providers#edit
+#              provider GET    /providers/:id(.:format)              providers#show
+#                       PATCH  /providers/:id(.:format)              providers#update
+#                       PUT    /providers/:id(.:format)              providers#update
+#                       DELETE /providers/:id(.:format)              providers#destroy
+#     electric_packages GET    /electric_packages(.:format)          electric_packages#index
+#                       POST   /electric_packages(.:format)          electric_packages#create
+#  new_electric_package GET    /electric_packages/new(.:format)      electric_packages#new
+# edit_electric_package GET    /electric_packages/:id/edit(.:format) electric_packages#edit
+#      electric_package GET    /electric_packages/:id(.:format)      electric_packages#show
+#                       PATCH  /electric_packages/:id(.:format)      electric_packages#update
+#                       PUT    /electric_packages/:id(.:format)      electric_packages#update
+#                       DELETE /electric_packages/:id(.:format)      electric_packages#destroy
+#                  root GET    /                                     home#index
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
