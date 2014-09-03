e7911414d32ef027eda26bc98b439164ad9754c4
dbushkanets <dbushkanets@harryfox.com>
2014-09-03 01:08:06 -0400

    put nav back in with color change on scroll

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,26 @@
+function colorNav() {
+  $(window).on("scroll", function(e) {
+    var top_of_middle_section = ($('.middle_section').position().top - 55);
+    if(scrollY > top_of_middle_section) {
+      $('.nav a, .navbar-brand')
+        .css("color", "#333")
+        .hover(function() {
+          $(this).css("color", "rgb(49, 240, 103)");
+        }, function() {
+          $(this).css("color", "#333");
+        });
+    } else {
+      $('.nav a, .navbar-brand')
+        .css("color", "white")
+        .hover(function() {
+          $(this).css("color", "rgb(49, 240, 103)");
+        }, function() {
+          $(this).css("color", "white");
+        });
+    }
+  });
+}
+
+$(function() {
+  colorNav();
+})
