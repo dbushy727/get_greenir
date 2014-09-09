a6d9f7c892761a8ea84f600fd1ff4d473bba9ed8
dbushkanets <dbushkanets@harryfox.com>
2014-09-09 00:28:54 -0400

    Added png icons for type of energy

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,25 +2,28 @@ function colorNav() {
   $(window).on("scroll", function(e) {
     var top_of_middle_section = ($('.second_section').position().top - 55);
     if(scrollY > top_of_middle_section) {
-      $('.nav a, .navbar-brand')
-        .css("color", "#333")
-        .hover(function() {
-          $(this).css("color", "rgb(49, 240, 103)");
-        }, function() {
-          $(this).css("color", "#333");
-        });
+      darkHeader();
     } else {
-      $('.nav a, .navbar-brand')
+      lightHeader();
+    }
+  });
+}
+
+function lightHeader() {
+  $('.nav a, .navbar-brand')
         .css("color", "white")
         .hover(function() {
           $(this).css("color", "rgb(49, 240, 103)");
         }, function() {
           $(this).css("color", "white");
         });
-    }
-  });
 }
-
-$(function() {
-  colorNav();
-})
+function darkHeader() {
+  $('.nav a, .navbar-brand')
+        .css("color", "#333")
+        .hover(function() {
+          $(this).css("color", "rgb(49, 240, 103)");
+        }, function() {
+          $(this).css("color", "#333");
+        });
+}
